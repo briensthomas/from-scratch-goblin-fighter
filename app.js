ad2d6dba39f62376471e34a34085eefce76c65ee
Brien Thomas <thomasbrien94@gmail.com>
2022-05-04 15:06:25 -0700

    Goblin form generates random new goblin if no name is provided, pushes to the goblins array

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 // import functions and grab DOM elements
-
+const goblinForm = document.getElementById('goblin-form');
 // let state
+let goblinsDefeated = 0;
 
+let goblinsArray = [
+    { name: 'Bob', hp: 5, },
+    { name: 'Scorio the Defiler, the Abhorred, Unmaker of Joy', hp: 1, },
+];
 // set event listeners 
+goblinForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const goblinData = new FormData(goblinForm);
+
+    let goblins = {
+        name: goblinData.get('goblin-name') || `Goblin #${Math.floor(Math.random() * 1000)}`,
+        hp: Math.ceil(Math.random() * 5),
+    };
+    goblinsArray.push(goblins);
+    console.log('logging goblins', goblinsArray);
+    // displayGoblinsList();
+    goblinForm.reset();
+});
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
